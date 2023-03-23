254e7d4cdc825fa50767bae0f6f1530223318cc9
m/s <m@umbr-ch.de>
2023-03-23 18:18:13 +0100

    streaming video for works

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ const strapiConfig = {
           Videos: {
             populate: "*",
           },
-          streamingVideo: { populate: "*" },
+          streamingVideo: "*",
         },
       },
     },
