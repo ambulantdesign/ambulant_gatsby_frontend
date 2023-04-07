e161b3a71aab050975337dbc5e5f0085daedca55
m/s <m@umbr-ch.de>
2023-04-07 20:21:29 +0200

    full-text search version 0.2

--- gatsby-config.js
+++ gatsby-config.js
@@ -133,7 +133,7 @@ module.exports = {
       resolve: `gatsby-plugin-algolia`,
       options: {
         appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
         queries: require("./src/utils/algolia"),
         chunkSize: 10000, // default: 1000,
         enablePartialUpdates: true,
