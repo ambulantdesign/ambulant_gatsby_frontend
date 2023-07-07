4e07a35fe9dbb892a97f7b76001925ad18b2a369
m/s <m@umbr-ch.de>
2023-07-07 13:50:20 +0200

    Short author name added (Gabriele Götz)

--- src/hooks/use-site-metadata.js
+++ src/hooks/use-site-metadata.js
@@ -8,6 +8,7 @@ export const useSiteMetadata = () => {
           siteMetadata {
             title
             author
+            authorShort
             city
             phone
             email
