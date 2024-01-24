a7a8e9ac7220468456c6928fd7a5ec3a745804fb
m/s <m@umbr-ch.de>
2024-01-24 06:55:25 +0100

    Using Local Fonts

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,6 +5,58 @@
  */
 // require("./src/assets/css/global.css")
 
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: `en` })
+  setHeadComponents([
+    [
+      <link
+        key="karla-300"
+        rel="preload"
+        href="/fonts/karla-v31-latin_latin-ext-300.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />,
+      <link
+        key="karla-300-italic"
+        rel="preload"
+        href="/fonts/karla-v31-latin_latin-ext-300italic.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />,
+      <link
+        key="karla-400"
+        rel="preload"
+        href="/fonts/karla-v31-latin_latin-ext-regular.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />,
+      <link
+        key="karla-400-italic"
+        rel="preload"
+        href="/fonts/karla-v31-latin_latin-ext-italic.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />,
+      <link
+        key="karla-700"
+        rel="preload"
+        href="/fonts/karla-v31-latin_latin-ext-700.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />,
+      <link
+        key="karla-700-italic"
+        rel="preload"
+        href="/fonts/karla-v31-latin_latin-ext-700italic.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />,
+    ],
+  ])
 }
