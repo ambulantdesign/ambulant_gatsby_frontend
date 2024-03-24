b98826d8a6612e25e76406080b27b5059b875ab8
m/s <m@umbr-ch.de>
2024-03-24 18:35:18 +0100

    added new sort order to list views (for works related to an art gallery)

--- src/utils/gallery-helper.js
+++ src/utils/gallery-helper.js
@@ -21,7 +21,7 @@ export const randomGalleryItem = gallery => {
 export const artGalleryListOrder = (
   projects,
   institutions,
-  sortOrder = "begin"
+  sortOrder = "end"
 ) => {
   let newProjectsList = []
 
